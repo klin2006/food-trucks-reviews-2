4d7aec719f39d8bd57c2fd706d15354bb27ef15c
Katherine Lin <50242222+klin2006@users.noreply.github.com>
2019-07-16 20:31:48 -0400

    Revert "Nichole"

--- src/main/resources/static/js/server-requests.js
+++ src/main/resources/static/js/server-requests.js
@@ -26,9 +26,7 @@ for (i = 0; i < dropdown.length; i++) {
 const tagAddButton = document.querySelector('.add-tag button');
 const tagAddInput = document.querySelector('.add-tag input');
 const tagsList = document.querySelector('.tags-list ul');
-const commentAddButton = document.querySelector('.add-comment button')
-const commentAddInput = document.querySelector('.add-comment input')
-
+const foodtruckId = document.querySelector('.foodtruckId');
 
 const xhr = new XMLHttpRequest()
 xhr.onreadystatechange = function(){
@@ -63,36 +61,3 @@ function removeTag(id){
     xhr.open('POST', '/tags/remove/' + id, true);
     xhr.send();
 }
-
-// const xhr = new XMLHttpRequest()
-// xhr.onreadystatechange = function(){
-//     if(xhr.readyState === 4 && xhr.status === 200){
-//     const res = xhr.responseText;
-//     commentAddInput.innerHTML = res;
-//     }
-// }
-
-// commentAddButton.addEventListener('click', function(){
-//     postTags(commentAddInput.value);
-//     console.log(commentAddInput.value);
-//     commentAddInput.value = "";
-// })
-
-// tagsList.addEventListener('click', function(event){
-//     if(event.target.classList.contains('x')){
-//     let tagId = event.target.previousElementSibling.previousElementSibling.value;
-//       console.log(tagId);
-//       removeTag(tagId);
-//     }
-// })
-
-
-// function postTags(tagType){
-//     xhr.open('POST', '/comments/' + tagType, true);
-//     xhr.send();
-// }
-
-// function removeTag(id){
-//     xhr.open('POST', '/comments/remove/' + id, true);
-//     xhr.send();
-// }
